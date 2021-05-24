8ac4238ca69b100b5fd15b3c07feeeab208ebc65
Okra Livia <okra.livia96@gmail.com>
2021-05-24 21:58:51 +0200

    commented out back to top btn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
+/*import ScrollArrow from './ScrollUp';*/
 
 function Footer() {
 	return (
+		<>
+		{/*<Link to="/" className="up">
+				<img src="/images/up.svg" alt='uparrow' width="50" className='up_arrow'/>
+				<ScrollArrow />
+		</Link>*/}
 		<div className="footer-container">
-		<div>
-			<h4>
-				<span>Let's stay in touch! </span>
-				<a href = "mailto: okralivia@gmail.com" className="footer-link-items">okralivia@gmail.com</a>
-			</h4>
+			<div>
+				<h4>
+					<span>Let's stay in touch! </span>
+					<a href = "mailto: okralivia@gmail.com" className="footer-link-items">okralivia@gmail.com</a>
+				</h4>
+			</div>
+			<div>
+				<small className='website-rights'>Okra Livia Inkeri-Dimle © 2021</small>
+			</div>
 		</div>
-		<div>
-			<small className='website-rights'>Okra Livia Inkeri-Dimle © 2021</small>
-		</div>
-		</div>
+		</>
 	);
 }
 
