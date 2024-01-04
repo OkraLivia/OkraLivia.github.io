c893bb61ad1984ae337b94378f7187cc07700def
Okra Livia <okra.livia96@gmail.com>
2024-01-04 12:22:20 +0100

    update ui gallery

--- src/components/UiGallery.js
+++ src/components/UiGallery.js
@@ -5,11 +5,11 @@ const imgUrls = [
   './images/ui/daily_ui-01-web.png',
   './images/ui/daily_ui-02-web.png',
   './images/ui/daily_ui-09-web.png',
-  './images/ui/daily_ui-04-web.png',
+  './images/ui/daily_ui-04-web.png',/*
   './images/ui/daily_ui-05-web.png',
   './images/ui/daily_ui-06-web.png',
   './images/ui/daily_ui-07-web.png',
-  './images/ui/daily_ui-03-web.png',
+  './images/ui/daily_ui-03-web.png',*/
   './images/ui/404-page.gif'
 ];
 
@@ -18,10 +18,10 @@ const imgTexts = [
   'The promt was to design an E-commerce shop displaying a single item. I choose to work with a chair, for each color option the solid background colorblock changes color accordingly. I thought it was a fun challenge to create a design for both mobile and desktop.',
   'The promt was to design a hotel booking application, I wanted to do a clean design with a easy-going feeling.',
   'The promt was to design a music player. The backround is a blurred and enlarged version of the album cover and I made the track component round like a CD, behind the currently playing song is smaller images of the previous and next track.',
-  'The promt was to design a sign up page. I wanted to use bright but calm colors for this design. Maybe it is a sign up page for a spa?',
+  /*'The promt was to design a sign up page. I wanted to use bright but calm colors for this design. Maybe it is a sign up page for a spa?',
   'The promt was to design a user profile. Similar to the E-commerce shop, the colors are different depending on the image. The color theme changes to fit the uploaded image.',
   'The promt was to design a credit card checkout. I wanted to use peachy colors since they are colors that I am not very used to work with. I thought a shop selling bath and body related products would be suitable for the aesthetic.',
-  'The promt was to design a home monitoring dashboard. It consist of different components that are interchangeable and I created a light mode and a dark mode for this task.',
+  'The promt was to design a home monitoring dashboard. It consist of different components that are interchangeable and I created a light mode and a dark mode for this task.',*/
   'The promt was to design a 404 page. I wanted to make a fun design, the image is a gif of Vincent Vega from Pulp Fiction where he is looking around with a confused expression.'
   ];
 
